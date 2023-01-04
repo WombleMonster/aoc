3a719a44461435a291ba98013d12c59045aa876d
David Jones <djonesuk@gmail.com>
2023-01-04 13:34:00 +0000

    day  08

---
+++ day-08/Tree.java
@@ -0,0 +1,21 @@
+public class Tree {
+
+  public int height;
+
+  public boolean visible = false;
+
+  // viewing distances
+  public int north = 0;
+  public int east = 0;
+  public int south = 0;
+  public int west = 0;
+
+  public Tree(int height) {
+    this.height = height;
+  }
+
+  public int getScenicScore() {
+    return north * east * south * west;
+  }
+
+}
--- template/Main.java
+++ template/Main.java
@@ -1,36 +1,39 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-;
+import java.util.*;;
 
 public class Main {
     private static String filename = "input.txt";
+    private static List<String> input;
 
     public static void main(String args[]) {
         System.out.println("Day XX");
+
+        readInput();
 
         partOne();
         // partTwo();
     }
 
-    private static void partOne() {
-        int total = 0;
-        
+    private static void readInput() {
+        input = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                // do something clever
+                input.add(line);
             }
         } catch (IOException e) {
             System.out.println(e);
         }
+    }
 
-        System.out.println("total = " + total);
+    private static void partOne() {
+
     }
 
     private static void partTwo() {
 
     }
-
 
 }
